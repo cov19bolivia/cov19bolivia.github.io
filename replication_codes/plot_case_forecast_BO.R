4d0f93ca995ffdc8731f4d96fce4a25ca194c99a
Pablo Alfredo Cuba Borda <pcubaborda@gmail.com>
2021-05-12 21:04:19 -0400

    Fixed plot

--- replication_codes/plot_case_forecast_BO.R
+++ replication_codes/plot_case_forecast_BO.R
@@ -145,11 +145,11 @@
                 color = "gray", size=0.7)+
     geom_line(data=outofsample, aes(x=Fecha, y=ma7), color = "red3",linetype="solid", lwd = 0.4) +    
     geom_point() +
-    annotate("point", x = outofsample$Fecha, y = outofsample$ma7, colour = "red3",size=0.55,shape=21,fill = NA)+
-    annotate("text", x = as.Date(c(outofsample$Fecha[length(outofsample$Fecha)]+5)), y = outofsample$ma7[length(outofsample$Fecha)]-5, label = format(outofsample$Fecha[length(outofsample$Fecha)], format="%b/%d"), size = 1.35)+
-    annotate("text", x = as.Date(peakdate[peakdate_loc])-2, y = peakvalue*1.01, label = "promedio móvil 7-días",size = 2.0,  color="red3",hjust = 1)+
-    annotate("text", x = as.Date(peakdate[peakdate_loc])-2, y = peakwave*0.2, label = "Casos diarios",size = 2.0,  color="blue3",hjust = 1)+
-    annotate("text", x = as.Date(vintage)+5, y = max(df$top[df$date<as.Date(vintage)+21]), label = "Pronósticos",size = 2.5,  color="black",hjust = 0)+
+    annotate("point", x = outofsample$Fecha, y = outofsample$ma7, colour = "red3",size=0.5,shape=21,fill = NA)+
+    annotate("text", x = as.Date(c(outofsample$Fecha[length(outofsample$Fecha)]+6)), y = outofsample$ma7[length(outofsample$Fecha)]*0.97, label = format(outofsample$Fecha[length(outofsample$Fecha)], format="%b/%d"), size = 1.75)+
+    annotate("text", x = as.Date(peakdate[peakdate_loc])-2, y = peakvalue*1.01, label = "promedio móvil 7-días",size = 2.15,  color="red3",hjust = 1)+
+    annotate("text", x = as.Date(peakdate[peakdate_loc])-2, y = peakwave*0.2, label = "Casos diarios",size = 2.15,  color="blue3",hjust = 1)+
+    annotate("text", x = as.Date(vintage)+5, y = max(df$top[df$date<as.Date(vintage)+21]), label = "Pronósticos",size = 2.15,  color="black",hjust = 0)+
     scale_x_date(date_breaks = "3 weeks" ,date_labels = "%b %d",limits = as.Date(c("2020-12-01",format(as.Date(vintage)+21,"%Y-%m-%d"))))+
     labs(title = paste0(filename,': casos diarios de Covid-19'),
          subtitle = paste0("Pronósticos del ", format(as.Date(vintage)+1,"%d/%b"), ' al ', format(as.Date(vintage)+21,"%d/%b")),
